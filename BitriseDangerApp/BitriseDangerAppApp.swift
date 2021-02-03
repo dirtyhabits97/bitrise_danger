49b31328b31a5b53f1e8d6e49969f51bcfb1b683
dirtyhabits97 <gonzalorehu@gmail.com>
2021-02-03 18:07:12 -0500

    feature: created ios project

---
+++ BitriseDangerApp/BitriseDangerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitriseDangerAppApp.swift
+//  BitriseDangerApp
+//
+//  Created by user on 3/02/21.
+//
+
+import SwiftUI
+
+@main
+struct BitriseDangerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitriseDangerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BitriseDangerApp
+//
+//  Created by user on 3/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
